21ec815f92175144ad6f3a8858a5526b74f23823
Mui <sweetrain05@hotmail.com>
2023-06-03 19:11:15 -0700

    commit

--- index.js
+++ index.js
@@ -2,10 +2,19 @@
 
 // hide scroll bar when on top of the page
 window.addEventListener("scroll", function () {
-    var body = document.querySelector("body");
+    const body = document.querySelector("body");
     if (window.scrollY === 0) {
         body.classList.add("hide-scrollbar");
     } else {
         body.classList.remove("hide-scrollbar");
     }
+});
+
+// company logo carousel using flickity
+let logoCarousel = document.querySelector(".main-carousel");
+
+let flkty = new Flickity(logoCarousel, {
+    // options
+    cellAlign: "left",
+    contain: true,
 });
