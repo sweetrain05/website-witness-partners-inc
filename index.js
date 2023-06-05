5046d00422f819547f581cdb3a399a7ae8f23c24
Mui <sweetrain05@hotmail.com>
2023-06-05 13:17:44 -0700

    updated on June 5th

--- index.js
+++ index.js
@@ -14,11 +14,35 @@ window.addEventListener("scroll", function () {
 let logoCarousel = document.querySelector(".main-carousel");
 
 let flkty = new Flickity(logoCarousel, {
-    contain: true,
+    // contain: true,
     autoPlay: 1500,
     freeScroll: true,
     prevNextButtons: true,
     pageDots: true,
     wrapAround: true,
-    adaptiveHeight: true,
+    // adaptiveHeight: true,
+});
+
+// links for icons on footer (open in new window when clicked)
+const links = [
+    "https://www.facebook.com/witnesspartners.us",
+    "https://www.instagram.com/witness_creative/",
+    "https://www.linkedin.com/company/witness-creative-partners-inc/",
+];
+
+const linkedIcons = document.querySelectorAll(".links");
+linkedIcons.forEach((icon, i) => {
+    icon.addEventListener("click", () => {
+        window.open(links[i], "_blank");
+    });
+});
+
+// clear e-mail input area when it is focused & let original value appear when unfocused
+const emailInput = document.getElementById("mce-EMAIL");
+
+emailInput.addEventListener("focus", function () {
+    this.value = "";
+});
+emailInput.addEventListener("blur", function () {
+    this.value = "enter you email here";
 });
