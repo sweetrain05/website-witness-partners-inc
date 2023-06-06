e0ec64e8134caa9bab80879a2ddbc8c54a50169c
Mui <sweetrain05@hotmail.com>
2023-06-06 11:21:48 -0700

    updated on June 6th

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 "use strict";
 
-// hide scroll bar when on top of the page
-window.addEventListener("scroll", function () {
+// 👉 hide scroll bar when on top of the page
+window.addEventListener("scroll", () => {
     const body = document.querySelector("body");
     if (window.scrollY === 0) {
         body.classList.add("hide-scrollbar");
@@ -10,20 +10,55 @@ window.addEventListener("scroll", function () {
     }
 });
 
-// company logo carousel using flickity
-let logoCarousel = document.querySelector(".main-carousel");
+// 👉 Carousel of company logos using swiper.js starts here
+const swiper = new Swiper(".swiper", {
+    // Optional parameters
+    direction: "horizontal",
+    loop: true,
+    centeredSlides: true,
+    effect: "slide",
+    speed: 500,
+    updateOnWindowResize: true,
 
-let flkty = new Flickity(logoCarousel, {
-    // contain: true,
-    autoPlay: 1500,
-    freeScroll: true,
-    prevNextButtons: true,
-    pageDots: true,
-    wrapAround: true,
-    // adaptiveHeight: true,
+    autoplay: {
+        delay: 700,
+        pauseOnMouseEnter: true,
+    },
+
+    // If we need pagination
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+
+    // Default parameters
+    slidesPerView: 1,
+    spaceBetween: 10,
+    // Responsive breakpoints
+    breakpoints: {
+        // when window width is >= 480px
+        480: {
+            slidesPerView: 2,
+            spaceBetween: 100,
+        },
+        // when window width is >= 767px
+        767: {
+            slidesPerView: 3,
+            spaceBetween: 300,
+        },
+        // when window width is >= 1024px
+        1024: {
+            slidesPerView: 4,
+            spaceBetween: 300,
+        },
+    },
+});
+// activate carousel autoplay when window is resized
+window.addEventListener("resize", () => {
+    swiper.autoplay.start();
 });
 
-// links for icons on footer (open in new window when clicked)
+// 👉 links for icons on menu and footer(open in new window when clicked)
 const links = [
     "https://www.facebook.com/witnesspartners.us",
     "https://www.instagram.com/witness_creative/",
@@ -37,12 +72,88 @@ linkedIcons.forEach((icon, i) => {
     });
 });
 
-// clear e-mail input area when it is focused & let original value appear when unfocused
+// 👉 clear e-mail input area when it is focused & let original value appear when unfocused
 const emailInput = document.getElementById("mce-EMAIL");
 
-emailInput.addEventListener("focus", function () {
-    this.value = "";
+emailInput.addEventListener("focus", () => {
+    emailInput.value = "";
 });
-emailInput.addEventListener("blur", function () {
-    this.value = "enter you email here";
+emailInput.addEventListener("blur", () => {
+    emailInput.value = "enter you email here";
+});
+
+// 👉 show responsive menu bar when hamburger icon is clicked
+// create class to handle toggle activity
+class ToggleMenuHandler {
+    constructor(element) {
+        this.isActive = false;
+        this.element = element;
+    }
+
+    // method to change current toggle status & css display style accordingly
+    toggle() {
+        if (this.isActive) {
+            this.isActive = false;
+            this.element.style.display = "none";
+        } else {
+            this.isActive = true;
+            this.element.style.display = "flex";
+        }
+    }
+}
+
+// select all buttons that are involved in the toggle activity
+const menuToggleBtns = document.querySelectorAll(".menu-toggle");
+
+// select the element that you want to apply display toggle effect
+const menuOverlay = document.querySelector(".menu_container_overlay");
+
+// create instance of toggle menu
+let toggleMenu = new ToggleMenuHandler(menuOverlay);
+
+// create click event for each button that needs toggle feature
+menuToggleBtns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        e.preventDefault();
+        toggleMenu.toggle();
+    });
+});
+
+// 👉 create click event listener on all elements that has contact-us router.
+//     These will activate function changePageTo().
+const contactusRouters = document.querySelectorAll(".contactus-router");
+contactusRouters.forEach((router) => {
+    router.addEventListener("click", (e) => {
+        e.preventDefault();
+        changePageTo("contactUs");
+    });
+});
+
+// 👉 create a function that replaces contents of <main> tag based on given directory name.
+function changePageTo(directory) {
+    // Select all html tags with page class name.
+    const pages = document.querySelectorAll(".page");
+    // create a new hash map
+    const hashMap = {};
+    // Insert data into hashMap based on the <article> class names(index=1 on the list)
+    // HashMap data format : { '2nd class name on article tag' : <article class="correspondingClass">...</article> }
+    // HashMap data example : { home: <article class="home">...</article>, contactUS: <article class="contactus">...</article>}
+    for (let i = 0; i < pages.length; i++) {
+        let name = pages[i].classList[1];
+        hashMap[name] = pages[i];
+    }
+    // select <main> tag and replace innerHTML with directory passed as parameter
+    const mainPage = document.querySelector("main");
+    mainPage.innerHTML = hashMap[directory].outerHTML;
+}
+
+// 👉 clear contact form input area when it is focused & let original value appear when unfocused
+const formInput = document.querySelectorAll(".form_input");
+formInput.forEach((inputArea) => {
+    inputArea.addEventListener("focus", () => {
+        inputArea.value = "";
+    });
+    inputArea.addEventListener("blur", () => {
+        inputArea.value = "";
+    });
 });
